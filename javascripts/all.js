650cd09bd53c623700a99167568b318a22685c0a
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-08-23 21:23:55 -0700

    Automated commit at 2015-08-24 04:23:55 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -10760,7 +10760,8 @@ return jQuery;
 $(function() {
   var $navbar = $('.navbar');
   var $bars = $('.bars, .navbar-overlay');
-  var navToggle = function()  {
+  var $nestedNav = $('.navbar-navigation a:not(:last-child)');
+  var toggleActive = function()  {
     //Inactive class is required for CSS animations
     if (!($navbar.hasClass('navbar--active')) && !($navbar.hasClass('navbar--inactive'))) {
       $navbar.toggleClass('navbar--active');
@@ -10769,9 +10770,31 @@ $(function() {
       $navbar.toggleClass('navbar--active').toggleClass('navbar--inactive');
     }
   };
+  var toggleFixed = function()  {
+    if ($(window).scrollTop() > window.innerHeight * .5)  {
+      $navbar.addClass('navbar--fixed');
+      $navbar.removeClass('navbar--absolute');
+    }
+    else  {
+      $navbar.removeClass('navbar--fixed');
+      $navbar.addClass('navbar--absolute');
+    }
+  }
   $bars.click(function(e) {
-    //e.preventDefault;
-    navToggle();
+    toggleActive();
+  });
+  $nestedNav.click(function() {
+    if (!($(this).hasClass('navigation--nested--show')))  {
+      $(this).addClass('navigation--nested--show');
+    }
+    else  {
+      $(this).removeClass('navigation--nested--show');
+    }
+  });
+  //Because firing events every time a user scrolls is HELLA slow
+  toggleFixed = _.throttle(toggleFixed, 500);
+  $(window).scroll(function() {
+    toggleFixed();
   });
 });
 
